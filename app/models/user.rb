5cae08acf6c723259aaf83fe26959a544a5d1e09
tono_0123 <tono_0123@tono.local>
2016-05-18 19:36:24 +0900

    Fix after

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+
+	# validation
+	validates :company, presence: true
+	validates :sei, presence: true, length: { maximum: 50 }
+	validates :mei, presence: true, length: { maximum: 50 }
+	validates :postal_code, format: { with: /¥d/, message: "半角数字のみ入力できます"}
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :mail, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	validates :tel, presence: true, numericality: {only_integer: true} #, format: { with: /¥d/ }
+
 end
