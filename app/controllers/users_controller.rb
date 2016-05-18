633be65feb9b9669480b3eed1898b772c27c7313
tono_0123 <tono_0123@tono.local>
2016-05-18 18:53:51 +0900

    7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,19 +26,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    # after ##########################################################################
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
-    #     format.json { render :show, status: :created, location: @user }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
-    # after ##########################################################################
-
-
     # before ##########################################################################
     respond_to do |format|
 
